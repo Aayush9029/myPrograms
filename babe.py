512a214f7ae1a3b53e65468d5ffd0aa4824dc3cd
Aayush <aayushpokharel36@gmail.com>
2019-11-25 22:19:04 -0500

    vscode starter template added
    ooh, makes a template now yahh

--- babe.py
+++ babe.py
@@ -1,53 +1,177 @@
 #!/usr/local/bin/python3
 
-'''note the google and youtube feature should work out of the box, spotify idk'''
-
 import pyautogui
 import os
 from sys import argv
 import time
 
-school_url = "schoolswebsite.com"
-school_login = ['youremail@email.com']  # [1] == password but env will be used
+google_url = "https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+school_url = "https://byod.peelschools.org/"
+school_login = ['yourschoolemail@school.net']  # [1] == password but env will be used
 youtube_url = "https://www.youtube.com/results?search_query="
 google_url = "https://www.google.com/search?q="
 spotify_url = "https://open.spotify.com/"
-default_email = 'youremail@email.com'
+default_email = 'yourgmail@gmail.com'
 
 
 def help():
     print('''
-babe, version 0.3
+babe, version 0.1
 
 usage: babe [peel] [-yt --youtube] [-gg --google] [-site --website]
 
-    peel [query] 
+    peel [query]
         example: ./babe peel google docs
-        example: ./babe peel classroom 
+        example: ./babe peel classroom
 
     youtube [query]
-        example: ./babe youtube cats 
-        example: ./babe yt pwediepie 
+        example: ./babe youtube cats
+        example: ./babe yt pwediepie
 
     google [query]
-        example: ./babe google duckduckgo 
-        example: ./babe gg memes 
+        example: ./babe google duckduckgo
+        example: ./babe gg memes
 
     websites [url]
-        example: ./babe website google.com 
-        example: ./babe ws writecode.me 
+        example: ./babe website google.com
+        example: ./babe ws writecode.me
 
     ''')
 
 
 def clear():
+
     pyautogui.hotkey('command', 'a')
+    time.sleep(0.1)
     pyautogui.hotkey('del')
 
 
+def pressTab(number):
+    for i in range(number):
+        time.sleep(0.1)
+        pyautogui.press('tab')
+
+
 def openUrl(url):
-    os.system('open -a firefox  --args -private-window ' + url)
+    os.system('open -a firefox  --args -private-window ')
+    time.sleep(1.5)
+    pyautogui.hotkey('command', 't')
+    pyautogui.typewrite(url)
+    pyautogui.press('enter')
     time.sleep(5)
+
+
+def vscode(projectName):
+    print('Making a starter template in desktop named', projectName)
+    where = "~/Desktop/" + projectName
+    command = "mkdir " + where
+    os.system(command)
+    os.system("code "+where)
+    time.sleep(3)
+    pyautogui.hotkey('command', 'w')
+    time.sleep(0.05)
+
+    pyautogui.hotkey('command', 'n')
+    pyautogui.typewrite('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+</head>
+<body>
+</body>
+</html>
+    ''')
+    time.sleep(0.3)
+
+    pyautogui.hotkey('command', 's')
+    time.sleep(1)
+
+    pyautogui.typewrite("index.html")
+    time.sleep(0.3)
+    pyautogui.press('enter')
+
+    pyautogui.hotkey('command', 'n')
+    pyautogui.typewrite('''
+
+    *{
+    padding:0px;
+    margin:0px;
+
+    ''')  # it automatically closes the brackects
+    time.sleep(0.3)
+
+    pyautogui.hotkey('command', 's')
+    time.sleep(1)
+
+    pyautogui.typewrite("style.css")
+    time.sleep(0.3)
+    pyautogui.press('enter')
+    time.sleep(0.3)
+
+    pyautogui.hotkey('command', 'n')
+    time.sleep(0.3)
+
+    pyautogui.hotkey('command', 's')
+    time.sleep(0.3)
+
+    pyautogui.typewrite("main.js")
+    time.sleep(0.3)
+
+    pyautogui.press('enter')
+
+
+def trello(projectName='Default'):
+    os.system('open -a trello')
+    time.sleep(2)
+
+    pressTab(1)
+    for i in range(4):
+        pyautogui.hotkey('shift', 'tab')
+    pyautogui.press('enter')
+    pressTab(2)
+    pyautogui.press('enter')
+    pyautogui.typewrite(projectName)
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.typewrite('Need to do')
+    pyautogui.press('enter')
+    pyautogui.typewrite('Doing')
+    pyautogui.press('enter')
+    pyautogui.typewrite('Done')
+    pyautogui.press('enter')
+    pyautogui.typewrite('Might do')
+    pyautogui.press('enter')
+
+
+def googleDefault():
+    openUrl('https://www.google.com')
+    pressTab(8)
+    pyautogui.press('enter')
+
+
+def youtubeDefault():
+    openUrl('https://www.youtube.com')
+    time.sleep(2)
+    pressTab(8)
+    pyautogui.press('enter')
+
+    time.sleep(2)
+    pyautogui.typewrite(default_email)
+    pyautogui.press('enter')
+    time.sleep(2)
+    clear()
+    pyautogui.press('down')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+
+    # pyautogui.press('enter')
 
 
 def peelSchool(what):
@@ -57,30 +181,21 @@ def peelSchool(what):
     pyautogui.press('enter')
     time.sleep(2)
     pyautogui.press('enter')
-
-    for i in range(8):
-        time.sleep(0.25)
-        pyautogui.press('tab')
-
+    pressTab(8)
     pyautogui.press('enter')
     time.sleep(2)
-
     pyautogui.typewrite(what)
     time.sleep(1)
-
     pyautogui.press('enter')
 
 
 def spotify(song):
-
     pyautogui.press('enter')
     openUrl(spotify_url)
-    for i in range(3):
-        time.sleep(0.25)
-        pyautogui.press('tab')
+    pressTab(3)
     pyautogui.press('enter')
     time.sleep(4)
-    pyautogui.typewrite(default_email)
+    pyautogui.typewrite('aayushpokharel36@gmail.com')
     time.sleep(1)
     pyautogui.press('enter')
     time.sleep(1)
@@ -89,9 +204,7 @@ def spotify(song):
 
 # login ends now music time :)
     if(song == 'recent'):
-        for i in range(3):
-            time.sleep(0.25)
-            pyautogui.press('tab')
+        pressTab(3)
         pyautogui.press('enter')
 
     else:
@@ -123,17 +236,32 @@ if(argv[1] == 'peel'):
     else:
         peelSchool('')
 
-elif argv[1] == '--youtube' or argv[1] == '-yt' or argv[1] == 'youtube' or argv[1] == 'yt':
-    youtube(argv[2])
+elif argv[1] == 'youtube' or argv[1] == 'yt':
+    if(argv[2] == 'login'):
+        youtubeDefault()
+    else:
+        youtube(argv[2])
 
-elif argv[1] == '--google' or argv[1] == '-gg' or argv[1] == 'google' or argv[1] == 'gg':
-    google(argv[2])
+elif argv[1] == 'google' or argv[1] == 'gg':
+    if(argv[2] == 'login'):
+        googleDefault()
+    else:
+        google(argv[2])
 
-elif argv[1] == '--website' or argv[1] == '-site' or argv[1] == 'website' or argv[1] == 'site':
+elif argv[1] == 'ws' or argv[1] == 'website' or argv[1] == 'site':
     openUrl(argv[2])
 
-elif argv[1] == '--spotify' or argv[1] == '-spot' or argv[1] == 'spotify' or argv[1] == 'spot':
+elif argv[1] == 'spotify' or argv[1] == 'spot':
     spotify(argv[2])
+
+elif argv[1] == 'trello':
+    trello(argv[2])
+
+elif argv[1] == 'firefox':
+    openUrl('')
+
+elif argv[1] == 'code':
+    vscode(argv[2])
 
 
 elif argv[1] == '-h' or argv[1] == '--help':
